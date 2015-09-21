f03bd8d8cadff03b97673e30b2faf6661e3ad170
wkojiro1 <wkojiro@eazydrive.co.jp>
2015-09-21 08:49:06 +0000

    monolist first

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,6 +1,15 @@
 class OwnershipsController < ApplicationController
   before_action :logged_in_user
 
+# 初期設定
+Amazon::Ecs.options = {
+    :aWS_access_key_id  =>  ENV["AWS_ACCESS_KEY_ID"],
+    :aWS_secret_key     =>  ENV["AWS_SECRET_KEY_ID"],
+    :country            => :jp,
+}
+
+
+
   def create
     if params[:asin]
       @item = Item.find_or_initialize_by(asin: params[:asin])
@@ -12,12 +21,12 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
+        response =  Amazon::Ecs.item_lookup(ARGV[0], { :response_group => 'Medium' })
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
-
-      amazon_item       = response.items.first
+       
+      amazon_item       = response.items.first  
       @item.title        = amazon_item.get('ItemAttributes/Title')
       @item.small_image  = amazon_item.get("SmallImage/URL")
       @item.medium_image = amazon_item.get("MediumImage/URL")
@@ -25,6 +34,8 @@ class OwnershipsController < ApplicationController
       @item.detail_page_url = amazon_item.get("DetailPageURL")
       @item.raw_info        = amazon_item.get_hash
       @item.save!
+      else
+    puts("#{ARGV[0]}: products not found")
     end
 
     # TODO ユーザにwant or haveを設定する
